8d96a6562c7feba33761f72f9bf0b4e25c0f94d5
ceartas1 <ramilvagizov123@gmail.com>
2024-02-29 13:59:58 +0300

    efrgf

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
