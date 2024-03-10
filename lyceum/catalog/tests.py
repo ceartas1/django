33a23493e32b1c84360ed6a21fbd51df3beb19b1
ceartas1 <ramilvagizov123@gmail.com>
2024-03-10 20:33:56 +0300

    fff

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -6,9 +6,9 @@ from django.test import Client, TestCase
 
 class StaticURLTests(TestCase):
     def test_catalog_endpoint(self):
-        response = Client().get('/catalog')
+        response = Client().get('/catalog/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_catalogint_endpoint(self):
-        response = Client().get('/catalog/<int:pk>')
+        response = Client().get('/catalog/1/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
