1d17b2ed320eceb826ea31e6f293846dac61f943
ceartas1 <ramilvagizov123@gmail.com>
2024-03-10 18:33:56 +0300

    day bog zelenaya

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -1,11 +1,14 @@
+from http import HTTPStatus
+
+
 from django.test import Client, TestCase
 
 
 class StaticURLTests(TestCase):
     def test_catalog_endpoint(self):
         response = Client().get('/catalog')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_catalogint_endpoint(self):
         response = Client().get('/catalog/<int:pk>')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -1,7 +1,10 @@
+from http import HTTPStatus
+
+
 from django.test import Client, TestCase
 
 
 class StaticURLTests(TestCase):
     def test_homepage_endpoint(self):
         response = Client().get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
